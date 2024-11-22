2d56512fae8b6f996643bf07a157e7003735db88
Manuel de la Peña <mdelapenya@gmail.com>
2024-11-22 16:58:48 +0100

    feat: add first module

---
+++ modules/a/a.go
@@ -0,0 +1,46 @@
+package a
+
+import (
+	"log"
+	"os"
+
+	core "github.com/mdelapenya/release-drafter-monorepo"
+)
+
+type ALogger struct {
+	logger *log.Logger
+}
+
+func DoSomething(msg string) {
+	core.WithLogger(&ALogger{
+		logger: log.New(os.Stdout, "", log.LstdFlags),
+	})
+
+	core.Log.Info(msg)
+}
+
+func (l *ALogger) Debug(msg string) {
+	l.log("DEBUG", msg)
+}
+
+func (l *ALogger) Info(msg string) {
+	l.log("INFO", msg)
+}
+
+func (l *ALogger) Warn(msg string) {
+	l.log("WARN", msg)
+}
+
+func (l *ALogger) Error(msg string) {
+	l.log("ERROR", msg)
+}
+
+func (l *ALogger) Fatal(msg string) {
+	l.logger.SetPrefix("FATAL: ")
+	l.logger.Fatalln(msg)
+}
+
+func (l *ALogger) log(prefix string, msg string) {
+	l.logger.SetPrefix("[A] " + prefix + ": ")
+	l.logger.Println(msg)
+}
---
+++ modules/a/a_test.go
@@ -0,0 +1,9 @@
+package a_test
+
+import (
+	"github.com/mdelapenya/release-drafter-monorepo/modules/a"
+)
+
+func ExampleALogger() {
+	a.DoSomething("This is an info message")
+}
