11442562bfa2e5f1f0d43cbf5675f68261965680
Manuel de la Peña <mdelapenya@gmail.com>
2024-11-22 16:42:30 +0100

    chore: simplify library

--- main.go
+++ main.go
@@ -1,20 +1,6 @@
-package main
+package core
 
 var lg Logger
-
-func main() {
-	WithLogger(NewDefaultLogger())
-
-	c := &Core{}
-
-	c.Info("Hello, world!")
-}
-
-type Core struct{}
-
-func (c *Core) Info(msg string) {
-	lg.Info(msg)
-}
 
 // SetLogger allows consumers to set their own logger
 func WithLogger(customLogger Logger) {
